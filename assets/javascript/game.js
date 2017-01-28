2766b1c80fc5f7ef0e83dcfed7848534a2f490ce
Radhika Sivarajan <radhikaraj333@gmail.com>
2017-01-28 10:34:53 -0800

    Generating computer choice corrected

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,47 +3,53 @@ var options = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","
 var win = 0;
 var loss  = 0;
 var guessLeft = 9;
-
 var computerChoice = options[Math.floor(Math.random() * options.length)];
+var userLetters = new Array();
 
-if(guessLeft<=9){
-	var userLetters = new Array();
+window.onkeydown = function(event) {	    	
 
-	window.onkeydown = function(event) {	    	
+    var scoreboard;
+    var userGuess = event.key.toLowerCase();	    
+	guessLeft--;
+	userLetters.push(userGuess);
+	
 
-	    var userGuess = event.key.toLowerCase();	    
-		guessLeft--;
-		userLetters.push(userGuess);
-		var scoreboard;
+	//If key pressed is an alphabet
+	if (event.keyCode >= 65 && event.keyCode <= 90){
 
-		//If key pressed is an alphabet
-		if (event.keyCode >= 65 && event.keyCode <= 90){
+	
+		//If user guess is correct
+		if(userGuess==computerChoice){
+			win++;	
 
-		
-			//If key pressed is correct
-			if(userGuess==computerChoice){
-				win++;	
+			//Reset values of user input array, guessleft and computer choose another letter
+			userLetters.splice(0,userLetters.length);	
+			guessLeft = 9;
+			computerChoice = options[Math.floor(Math.random() * options.length)];
 
-				//Reset values of user input array guessleft
-				userLetters.splice(0,userLetters.length);	
-				guessLeft = 9;
+    	}
 
-	    	}
-	    	scoreboard = "Wins: " + win + "<br>Loss: " + loss + "<br>CC " + computerChoice+ "<br>Guess left: " + guessLeft + "<br>Your gues so far " + userLetters;
-	    	document.getElementById("score").innerHTML = scoreboard;
-		
-				
-			if (guessLeft<1){
-				loss++;
-				//Reset values of user input array guessleft
-				userLetters.splice(0,userLetters.length);
-				guessLeft = 9;
-			}
-			scoreboard = "Wins: " + win + "<br>Loss: " + loss + "<br>CC " + computerChoice+ "<br>Guess left: " + guessLeft + "<br>Your gues so far " + userLetters;
-			document.getElementById("score").innerHTML = scoreboard;
+    	//Update scoreboard
+    	scoreboard = "Wins: " + win + "<br>Loss: " + loss + "<br>Guess left: " + guessLeft + "<br>Your gues so far " + userLetters;
+    	document.getElementById("score").innerHTML = scoreboard;
+    	//console.log(computerChoice);
+			
+		//If guess left is 0
+		if (guessLeft<1){
+			loss++;
+
+			//Reset values of user input array, guessleft and computer choose another letter
+			userLetters.splice(0,userLetters.length);
+			guessLeft = 9;
+			computerChoice = options[Math.floor(Math.random() * options.length)];
 
 		}
 
-	};
-	
-}
+		//Update scoreboard
+		scoreboard = "Wins: " + win + "<br>Loss: " + loss + "<br>Guess left: " + guessLeft + "<br>Your gues so far " + userLetters;
+		document.getElementById("score").innerHTML = scoreboard;		
+		//console.log(computerChoice);
+	}
+
+};
+
